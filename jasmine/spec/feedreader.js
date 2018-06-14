b30b27ea94963d8c8ea2a07d6a3374d1e70c25ab
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-06-14 13:31:10 +0200

    13. New test suite Initial Entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,7 +84,12 @@ $(function() {
       });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
+      describe('Initial entries', function(){
 
+
+
+
+      });
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
